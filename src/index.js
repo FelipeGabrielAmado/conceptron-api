825bec36fa67263e920c153c5e1e42a7f8ce4c20
Felipe Gabriel Amado <felipegabrielamado@gmail.com>
2021-05-05 20:31:47 -0300

    Revertido alteração na porta

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.use(cors());
 // Routes
 app.use(require('./routes/index'));
 
-app.listen(process.env.PORT || 3000);
-console.log('Server on port', 3000);
+app.listen(5000);
+console.log('Server on port', 5000);
